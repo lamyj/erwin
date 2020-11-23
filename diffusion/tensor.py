a9f4b73ed66d5c8012b87302c23d6a8b249a3e20
Julien Lamy <lamy@unistra.fr>
2020-11-23 10:01:18 +0100

    Add tensor options

--- diffusion/tensor.py
+++ diffusion/tensor.py
@@ -3,7 +3,9 @@ import spire
 from .. import entrypoint
 
 class Tensor(spire.TaskFactory):
-    def __init__(self, source, target, mask=None):
+    def __init__(
+            self, source, target, 
+            mask=None, dkt=None, ols=False, iter=None, b0=None):
         spire.TaskFactory.__init__(self, target)
         self.file_dep = [source]
         if mask is not None:
@@ -12,6 +14,10 @@ class Tensor(spire.TaskFactory):
         self.actions = [
             ["dwi2tensor", "-force"] 
                 + (["-mask", mask] if mask else [])
+                + (["-dkt", dkt] if dkt else [])
+                + (["-ols"] if ols else [])
+                + (["-iter", str(iter)] if iter else [])
+                + (["-b0", b0] if b0 else [])
                 + [source, target]]
 
 def main():
@@ -19,4 +25,8 @@ def main():
         Tensor, [
             ("source", {"help": "Source DWI image"}),
             ("target", {"help": "Target diffusion tensor image"}),
-            ("--mask", {"help": "Binary mask"})])
+            ("--mask", {"help": "Binary mask"}),
+            ("--dkt", {"help": "Target diffusion kurtosis image"}),
+            ("--ols", {"action": "store_true", "help": "Perform initial fit with OLS"}),
+            ("--iter", {"help": "Number of iterative reweightings"}),
+            ("--b0", {"help": "Target estimated b=0 s/mm² image"})])
