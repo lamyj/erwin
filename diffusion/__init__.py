a87aca9733860243c95e7eb1e08d4a37531abdf5
Julien Lamy <lamy@unistra.fr>
2020-11-24 11:28:07 +0100

    Add single fiber response

--- diffusion/__init__.py
+++ diffusion/__init__.py
@@ -1,4 +1,5 @@
 from .mask import Mask
 from .preprocessing import Preprocessing
+from .single_fiber_response import SingleFiberResponse
 from .tensor import Tensor
 from .tensor_metric import TensorMetric
---
+++ diffusion/single_fiber_response.py
@@ -0,0 +1,27 @@
+import spire
+
+from .. import entrypoint
+
+class SingleFiberResponse(spire.TaskFactory):
+    def __init__(self, source, prefix, mask=None):
+        spire.TaskFactory.__init__(self, prefix+"*.response")
+        
+        self.file_dep = [source]
+        if mask is not None:
+            self.file_dep.append(mask)
+        
+        self.targets = [
+            "{}{}.response".format(prefix, x) for x in ["wm", "gm", "csf"]]
+        
+        self.actions = [
+            # DWI-based response estimation
+            # As of Mrtrix 3.0.0, this is the "improved" Dhollander method.
+            ["dwi2response", "dhollander", "-force"]
+                + [source] + (["-mask", mask] if mask else []) + self.targets]
+
+def main():
+    return entrypoint(
+        SingleFiberResponse, [
+            ("source", {"help": "Source DWI image"}),
+            ("prefix", {"help": "Prefix of the target response files"}),
+            ("--mask", {"help": "Binary mask"})])
