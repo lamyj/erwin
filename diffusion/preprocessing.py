c752cc371392619937c8f950cb5afdcd1c5292f5
Julien Lamy <lamy@unistra.fr>
2020-12-03 14:34:12 +0100

    Fix paths of temp files

--- diffusion/preprocessing.py
+++ diffusion/preprocessing.py
@@ -16,26 +16,33 @@ class Preprocessing(spire.TaskFactory):
     def __init__(self, source, target):
         spire.TaskFactory.__init__(self, str(target))
         
-        temp = "__{}_temp.mif".format(os.path.basename(target))
-        temp2 = "__{}_temp2.mif".format(os.path.basename(target))
+        temp = os.path.join(
+            os.path.dirname(target), 
+            "__{}_temp.mif".format(os.path.basename(target)))
+        temp2 = os.path.join(
+            os.path.dirname(target), 
+            "__{}_temp2.mif".format(os.path.basename(target)))
+        temp_dir = os.path.join(
+            os.path.dirname(target), 
+            "__{}_dwipreproc.tmp".format(os.path.basename(target)))
         
         self.file_dep = [source]
         self.targets = [target]
         self.actions = [
-        ["dwidenoise", "-force", source, temp],
-        # WARNING: must NOT be stored in the same file
-        ["mrdegibbs", "-force", temp, temp2],
-        [
-            "dwifslpreproc", "-force",
-            "-scratch", "dwipreproc.tmp", "-rpe_header", 
-            # WARNING: eddy complains that data is not shelled even after using
-            # the "ideal" b-values (which we don't).
-            "-eddy_options", " --slm=linear --data_is_shelled",
-            temp2, temp],
-        ["dwibiascorrect", "ants", "-force", temp, target],
-        # Clean-up
-        ["rm", "-rf", temp, temp2, "dwipreproc.tmp"]
-    ]
+            ["dwidenoise", "-force", source, temp],
+            # WARNING: must NOT be stored in the same file
+            ["mrdegibbs", "-force", temp, temp2],
+            [
+                "dwifslpreproc", "-force",
+                "-scratch", temp_dir, "-rpe_header", 
+                # WARNING: eddy complains that data is not shelled even after using
+                # the "ideal" b-values (which we don't).
+                "-eddy_options", " --slm=linear --data_is_shelled",
+                temp2, temp],
+            ["dwibiascorrect", "ants", "-force", temp, target],
+            # Clean-up
+            ["rm", "-rf", temp, temp2, "dwipreproc.tmp"]
+        ]
 
 def main():
     return entrypoint(
