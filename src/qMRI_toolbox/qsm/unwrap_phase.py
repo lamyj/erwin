f906072ed2a35950e9d8f13376700f81f8571e3a
Julien Lamy <lamy@unistra.fr>
2021-01-28 12:33:29 +0100

    Fix unwrap_phase for 3D images and older scipy

--- src/qMRI_toolbox/qsm/unwrap_phase.py
+++ src/qMRI_toolbox/qsm/unwrap_phase.py
@@ -8,6 +8,17 @@ import spire
 
 from .. import entrypoint
 
+# scipy.fft has a parallel version while numpy.fft does not. Older version of
+# scipy do not have this feature.
+try:
+    scipy.fft.fftn
+except:
+    fft = numpy.fft.fftn
+    ifft = numpy.fft.ifftn
+else:
+    fft = lambda x: scipy.fft.fftn(x, workers=4)
+    ifft = lambda x: scipy.fft.ifftn(x, workers=4)
+
 class UnwrapPhase(spire.TaskFactory):
     """ Unwrap the phase image.
         
@@ -37,9 +48,9 @@ class UnwrapPhase(spire.TaskFactory):
         except FileNotFoundError:
             meta_data = None
         
+        # Read the phase image and scale the values if needed
         phi_w_image = nibabel.load(source_path)
         phi_w = phi_w_image.get_fdata()
-        
         # Siemens phase images are mapping [-π, +π[ to [-4096, +4094]
         if (
                 meta_data is not None
@@ -47,18 +58,11 @@ class UnwrapPhase(spire.TaskFactory):
                 and meta_data["Manufacturer"][0] == "SIEMENS"):
             phi_w *= numpy.pi / 4096
         
-        # scipy.fft has a parallel version while numpy.fft does not.
-        fft = lambda x: scipy.fft.fftn(x, workers=4)
-        ifft = lambda x: scipy.fft.ifftn(x, workers=4)
-        
-        # Pad on all spatial dimensions
-        phi_w = numpy.pad(phi_w, 3*[2*[padding]]+[[0,0]], mode="constant")
-        
-        # Compute the k-space coordinates and their norm
+        # Compute the padded k-space coordinates and their norm
         k = numpy.array(
             numpy.meshgrid(
                 *[
-                    numpy.fft.fftfreq(n, d) 
+                    numpy.fft.fftfreq(n+2*padding, d) 
                     for n,d in zip(
                         phi_w.shape[:3], phi_w_image.header["pixdim"][1:4])],
                 indexing="ij"
@@ -67,31 +71,41 @@ class UnwrapPhase(spire.TaskFactory):
         
         # Process volume by volume as we need to allocate large complex-double
         # arrays for the FFT
-        phi_prime = numpy.empty(phi_w_image.shape, float)
-        for i in range(phi_w.shape[3]):
-            cos_phi_w = numpy.cos(phi_w[..., i])
-            sin_phi_w = numpy.sin(phi_w[..., i])
-            laplacian = (
-                cos_phi_w * ifft(norm * fft(sin_phi_w))
-                - sin_phi_w * ifft(norm * fft(cos_phi_w)))
-
-            # Avoid division by 0. This also centers the mean phase of the
-            # image.
-            old = norm[0,0,0]
-            norm[0,0,0] = 1
-            phi_volume = fft(laplacian)
-            phi_volume /= norm
-            phi_volume[0,0,0] = 0
-            norm[0,0,0] = old
-
-            phi_volume = ifft(phi_volume)
-
-            cropped = tuple(3*[slice(padding, -padding)])
-            phi_prime[..., i] = phi_volume[cropped].real
+        phi_prime = numpy.empty(phi_w.shape, float)
+        if phi_prime.ndim == 4:
+            for i in range(phi_w.shape[3]):
+                phi_prime[..., i] = UnwrapPhase._unwrap_volume(
+                    phi_w[..., i], norm, padding)
+        else:
+            phi_prime = UnwrapPhase._unwrap_volume(phi_w, norm, padding)
         
         nibabel.save(
             nibabel.Nifti1Image(phi_prime, phi_w_image.affine), target_path)
-
+    
+    @staticmethod
+    def _unwrap_volume(phi_w, norm, padding):
+        phi_w = numpy.pad(phi_w, 3*[2*[padding]], mode="constant")
+        
+        cos_phi_w = numpy.cos(phi_w)
+        sin_phi_w = numpy.sin(phi_w)
+        laplacian = (
+            cos_phi_w * ifft(norm * fft(sin_phi_w))
+            - sin_phi_w * ifft(norm * fft(cos_phi_w)))
+        
+        # Avoid division by 0. This also centers the mean phase of the
+        # image.
+        old = norm[0,0,0]
+        norm[0,0,0] = 1
+        phi_prime = fft(laplacian)
+        phi_prime /= norm
+        phi_prime[0,0,0] = 0
+        norm[0,0,0] = old
+        
+        phi_prime = ifft(phi_prime)
+        
+        roi = tuple(3*[slice(padding, -padding)])
+        return phi_prime[roi].real
+    
 def main():
     return entrypoint(
         UnwrapPhase, [
