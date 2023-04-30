af2fd2763e985cc71f1fd1992ac83cf433b64f08
Julien Lamy <lamy@unistra.fr>
2023-04-30 09:52:41 +0200

    Make diffusion.SphericalDeconvolutionResponse more modular

--- src/erwin/diffusion/spherical_deconvolution_response.py
+++ src/erwin/diffusion/spherical_deconvolution_response.py
@@ -6,28 +6,34 @@ from ..cli import *
 class SphericalDeconvolutionResponse(spire.TaskFactory):
     """ Estimation of WM, GM and CSF response functions.
         
-        This wraps "dwi2response dhollander" from MRtrix3.
+        This wraps dwi2response from MRtrix3.
     """
     
-    def __init__(self, source: str, prefix: str, mask: Optional[str]=None):
+    def __init__(
+            self, source: str, target: str, mask: Optional[str]=None,
+            algorithm: Optional[Choice["tournier", "dhollander"]]="dhollander"):
         """ :param source: Path to the source diffusion-weighted image
-            :param prefix: Prefix of the targets response files
+            :param target: Prefix (dhollander) or path (tournier) to the response
             :param mask: Path to the binary mask
+            :param algorithm: Name of the estimation algorithm 
         """
         
-        spire.TaskFactory.__init__(self, prefix+"*.response")
+        spire.TaskFactory.__init__(
+            self,
+            str(target+"wm.response" if algorithm=="dhollander" else target))
         
         self.file_dep = [source]
         if mask is not None:
             self.file_dep.append(mask)
         
-        self.targets = [
-            "{}{}.response".format(prefix, x) for x in ["wm", "gm", "csf"]]
+        if algorithm == "dhollander":
+            self.targets = [
+                "{}{}.response".format(target, x) for x in ["wm", "gm", "csf"]]
+        else:
+            self.targets = [target]
         
         self.actions = [
-            # DWI-based response estimation
-            # As of Mrtrix 3.0.0, this is the "improved" Dhollander method.
-            ["dwi2response", "dhollander", "-force"]
+            ["dwi2response", algorithm, "-force"]
                 + [source] + (["-mask", mask] if mask else []) + self.targets]
 
 def main():
