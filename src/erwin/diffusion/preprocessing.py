73167af4671fc84d9da68a2f8f87ef6e906b3d37
Julien Lamy <lamy@unistra.fr>
2023-04-30 09:21:21 +0200

    Make diffusion.Preprocessing more modular

--- src/erwin/diffusion/preprocessing.py
+++ src/erwin/diffusion/preprocessing.py
@@ -1,5 +1,6 @@
-import os
+import pathlib
 import subprocess
+import tempfile
 
 import spire
 
@@ -15,38 +16,57 @@ class Preprocessing(spire.TaskFactory):
         information and diffusion-encoding scheme).
     """
     
-    def __init__(self, source: str, target: str):
+    def __init__(
+            self, source: str, target: str,
+            no_preprocess: Flag=False, no_bias_correction: Flag=False):
         """ :param source: Path to source diffusion-weighted image
             :param target: Path to the target preprocessed DWI image
+            :param no_preprocess: Skip Eddy/Topup
+            :param no_bias_correction: Skip bias correction
         """
         
         spire.TaskFactory.__init__(self, str(target))
         
-        target_dir = os.path.dirname(target)
-        target_base = os.path.basename(target)
-        
-        temp = os.path.join(target_dir, "__{}_temp.mif".format(target_base))
-        temp2 = os.path.join(target_dir, "__{}_temp2.mif".format(target_base))
-        temp_dir = os.path.join(
-            target_dir, "__{}_dwipreproc.tmp".format(target_base))
-        
         self.file_dep = [source]
         self.targets = [target]
         self.actions = [
-            ["dwidenoise", "-force", source, temp],
-            # WARNING: must NOT be stored in the same file
-            ["mrdegibbs", "-force", temp, temp2],
-            [
-                "dwifslpreproc", "-force",
-                "-scratch", temp_dir, "-rpe_header", 
-                # WARNING: eddy complains that data is not shelled even after using
-                # the "ideal" b-values (which we don't).
-                "-eddy_options", " --slm=linear --data_is_shelled",
-                temp2, temp],
-            ["dwibiascorrect", "ants", "-force", temp, target],
-            # Clean-up
-            ["rm", "-rf", temp, temp2, "dwipreproc.tmp"]
-        ]
+            (
+                Preprocessing.preprocess,
+                (source, target, no_preprocess, no_bias_correction))]
+    
+    @staticmethod
+    def preprocess(source, target, no_preprocess, no_bias_correction):
+        with tempfile.TemporaryDirectory() as dir:
+            dir = pathlib.Path(dir)
+            
+            denoised = dir/"denoised.mif"
+            subprocess.check_call(["dwidenoise", "-force", source, denoised])
+            
+            if no_preprocess and no_bias_correction:
+                degibbsed = target
+            else:
+                degibbsed = dir/"degibbsed.mif"
+            # WARNING: must NOT be stored in the same file
+            subprocess.check_call(["mrdegibbs", "-force", denoised, degibbsed])
+            
+            if no_bias_correction:
+                preprocessed = target
+            else:
+                preprocessed = dir/"preprocessed.mif"
+            if not no_preprocess:
+                subprocess.check_call([
+                    "dwifslpreproc", "-force",
+                    "-scratch", dir, "-rpe_header", 
+                    # WARNING: eddy complains that data is not shelled even
+                    # after using the "ideal" b-values (which we don't).
+                    "-eddy_options", " --slm=linear --data_is_shelled",
+                    degibbsed, preprocessed])
+            else:
+                preprocessed = degibbsed 
+            
+            if not no_bias_correction:
+                subprocess.check_call([
+                    "dwibiascorrect", "ants", "-force", preprocessed, target])
 
 def main():
     return entrypoint(Preprocessing)
